0275e5ca3ce6f6531052ae05111e9b5e7c9f08e2
Brian Moriguchi <brian@beckos.com>
2020-09-07 16:30:54 -0700

    Initial Commit

---
+++ WatchOS Text Input/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchOS Text Input
+//
+//  Created by Tatsuya Moriguchi on 9/7/20.
+//  Copyright © 2020 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
